ad0f86228d81f5a7681758d2a28f45071e34a1a0
Ethan Fajnkuchen <102037640+EthanFajnkuchen@users.noreply.github.com>
2024-01-10 10:41:18 +0200

    fix bug in js for navbar

--- js/header.js
+++ js/header.js
@@ -1,13 +1,15 @@
 document.addEventListener('DOMContentLoaded', function() {
     const navLinks = document.querySelectorAll('.navbar-nav .nav-link');
+    const logo = document.querySelector('.navbar-brand');
 
     navLinks.forEach(link => {
         link.addEventListener('click', function() {
-            // Remove the class from all nav links
             navLinks.forEach(lnk => lnk.classList.remove('active-nav-link'));
-
-            // Add the class to the clicked nav link
             this.classList.add('active-nav-link');
         });
+    });
+
+    logo.addEventListener('click', function() {
+        navLinks.forEach(link => link.classList.remove('active-nav-link'));
     });
 });
