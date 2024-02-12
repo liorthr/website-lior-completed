b5f29440a68616e911ef647a89f2f5b34c6a55ad
Ethan Fajnkuchen <102037640+EthanFajnkuchen@users.noreply.github.com>
2024-02-12 12:23:30 +0200

    add dropdown for client/pro + prevent user to submit more than once at a time

--- js/submitForm.js
+++ js/submitForm.js
@@ -1,24 +1,37 @@
 document.addEventListener('DOMContentLoaded', function() {
     var form = document.getElementById('contactForm');
+    var selectElement = document.querySelector('#contactForm select[name="status"]');
+    var submitButton = form.querySelector('button[type="submit"]'); // Select the submit button
 
-    // Add an event listener for the 'submit' event on the form
     form.addEventListener('submit', function(event) {
         event.preventDefault(); // Prevent the default form submission action
 
+        submitButton.disabled = true; // Disable the submit button
+
         // Gather the form data
         var formData = new FormData(form);
-        console.log(formData);
-        // Perform the fetch request to the Google Apps Script Web App URL
+
         fetch('https://script.google.com/macros/s/AKfycbzLrr2wzXeuQQyYH1VOXw4st2jlOHxUEFVTfiwIAnUrD23gwBmn86lcg7Cf1XdObKc/exec', {
             method: 'POST',
             body: formData,
-    })
-    .then(() => {
-        form.reset();        
-    }).catch(error => {
-        alert('Something went wrong, please try again later!')
-        console.error('Error:', error);
-        // Handle any errors that occur during fetch
+        })
+        .then(() => {
+            form.reset();        
+            selectElement.style.color = '#757575'; // Reset select color to default
+            submitButton.disabled = false; // Re-enable the submit button
+        }).catch(error => {
+            console.error('Error:', error);
+            alert('Something went wrong, please try again later!');
+            submitButton.disabled = false; // Re-enable the submit button even if there's an error
+        });
     });
-});
+
+    // Change the color of the select element when a non-disabled option is selected
+    selectElement.addEventListener('change', function() {
+        if(this.value) { // Checks if a value is selected
+            this.style.color = 'black'; // Change color to black if an option is selected
+        } else {
+            this.style.color = '#757575'; // Revert to the default color if the placeholder is selected again
+        }
+    });
 });
